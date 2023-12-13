43d789105aea7acad24385c149f9f0ad25839ef3
hertogp <git.hertogp@gmail.com>
2023-12-13 11:35:52 +0100

    adds mix iana.update trust, only checksums are checked at this point

--- lib/mix/task/iana.root_hints.ex
+++ lib/mix/task/iana.root_hints.ex
@@ -1,9 +1,11 @@
-defmodule Mix.Tasks.Iana.Update do
+defmodule Mix.Tasks.Iana.Root do
   @moduledoc """
-  Retrieve the root hints from Iana.
+  Retrieves IANA's root hints and/or trust anchor.
 
-  Saves the root hints to `priv/named.root` and creates
-  `priv/rrs.root` containing NS resource records.
+  Use:
+  - `mix iana.root hints` to update local copy of IANA's root nameservers
+  - `mix iana.root trust` to update local copy of IANA's trust anchor
+  - `mix iana.root all` to do both
 
   """
   @shortdoc "Retrieves Iana's root hints"
@@ -11,6 +13,7 @@ defmodule Mix.Tasks.Iana.Update do
 
   @priv :code.priv_dir(:dnscheck)
   @user_agent {'User-agent', 'Elixir Dnscheck'}
+
   # httpc options
   @cacerts :public_key.cacerts_get()
   @ssl_opts [{:verify, :verify_peer}, {:depth, 99}, {:cacerts, @cacerts}]
@@ -18,57 +21,67 @@ defmodule Mix.Tasks.Iana.Update do
   @opts [body_format: :binary]
 
   # root hints
-  @root_hints_url "https://www.internic.net/domain/named.root"
+  @root_hints "https://www.internic.net/domain/named.root"
   @fname_root Path.join([@priv, "named.root"])
   @fname_rrs Path.join([@priv, "rrs.root"])
 
   # trust anchors
-  @trust_crc "https://data.iana.org/root-anchors/checksums-sha256.txt"
-  @trust_pem "https://data.iana.org/root-anchors/icannbundle.pem"
-  @trust_sig "https://data.iana.org/root-anchors/root-anchors.p7s"
-  @trust_xml "https://data.iana.org/root-anchors/root-anchors.xml"
-  @fname_crc Path.join(@priv, "checksums-sha256.txt")
-  @fname_pem Path.join(@priv, "icannbundle.pem")
-  @fname_sig Path.join(@priv, "root-anchors.p7s")
-  @fname_xml Path.join(@priv, "root-anchors.xml")
+  @trust_url "https://data.iana.org/root-anchors"
+  @fname_chk Path.join(@priv, "checksums-sha256.txt")
+  @dnskey "https://dns.google.com/resolve?name=.&type=dnskey"
 
+  # Notes
+  # % openssl smime -verify -CAfile icannbundle.pem -inform der -in root-anchors.p7s -content root-anchors.xml
+  # -> verification successful
   @impl Mix.Task
   def run(args) do
     case args do
       ["hints"] -> hints()
       ["trust"] -> trust()
-      _ -> Mix.shell().info("expected one of 'hints', 'trust'")
+      ["all"] -> all()
+      [] -> all()
+      huh -> Mix.shell().info("\n#{huh} not supported, try: 'hints', 'trust', 'all' or nothing")
     end
   end
 
-  # [[ TRUST ]]
+  # [[ ALL ]]
+  defp all() do
+    hints()
+    trust()
+  end
+
+  # [[ TRUST ANCHORS ]]
   defp trust() do
-    Mix.shell().info("Updating trust anchors")
+    Mix.shell().info("Checking trust anchor")
 
-    new_crc = fetch(@trust_crc)
-    old_crc = read_file(@fname_crc)
+    fname = "checksums-sha256.txt"
+    new_chk = fetch("#{@trust_url}/#{fname}")
+    old_chk = read_file(@fname_chk)
 
-    if old_crc == new_crc do
-      Mix.shell().info("Checksum's have not changed, Trust is up to date")
+    if old_chk == new_chk do
+      Mix.shell().info(" - local checksums match the remote checkums")
+      Mix.shell().info(" - local trust is up to date")
     else
-      Mix.shell().info("Saving #{@fname_crc}")
-      File.write!(@fname_crc, new_crc)
-      new_pem = fetch(@trust_pem)
-      Mix.shell().info("Saving #{@fname_pem}")
-      File.write!(@fname_pem, new_pem)
-      new_sig = fetch(@trust_sig)
-      Mix.shell().info("Saving #{@fname_sig}")
-      File.write!(@fname_sig, new_sig)
-      new_xml = fetch(@trust_xml)
-      Mix.shell().info("Saving #{@fname_xml}")
-      File.write!(@fname_xml, new_xml)
+      Mix.shell().info(" - local checksums do not match the remote checksums")
+      Mix.shell().info(" - saving new checksums to #{fname}")
+      File.write!(@fname_chk, new_chk)
+      Mix.shell().info(" - updating local trust anchor")
+
+      new_chk
+      |> String.split("\n", trim: true)
+      |> Enum.map(fn s -> String.split(s) end)
+      |> Enum.reduce(%{}, fn [chksum, fname], acc ->
+        Map.put(acc, String.downcase(fname), chksum)
+      end)
+      |> Enum.map(fn {fname, chksum} -> check(fname, chksum) end)
     end
   end
 
-  # [[ HINTS ]]
+  # [[ ROOT HINTS ]]
 
   defp hints() do
-    new_body = fetch(@root_hints_url)
+    Mix.shell().info("Checking root hints")
+    new_body = fetch(@root_hints)
     new_serial = get_serial(new_body)
     new_rrs = hints_to_rrs(new_body)
 
@@ -76,8 +89,11 @@ defmodule Mix.Tasks.Iana.Update do
     old_serial = get_serial(old_body)
     old_rrs = hints_to_rrs(old_body)
 
+    Mix.shell().info(" - remote serial #{inspect(new_serial)}")
+    Mix.shell().info(" - local  serial #{inspect(old_serial)}")
+
     if old_serial == new_serial do
-      Mix.shell().info("Serial #{old_serial}, root hints up to date")
+      Mix.shell().info(" - root hints up to date")
 
       # just to be sure
       unless File.exists?(@fname_rrs),
@@ -98,7 +114,7 @@ defmodule Mix.Tasks.Iana.Update do
         |> Enum.map(fn rr -> " - #{rr.name}\t#{rr.ttl}\t#{rr.type}\t#{rr.rdmap.ip}" end)
         |> Enum.join("\n")
 
-      Mix.shell().info("\nUpdated serial #{old_serial} -> #{new_serial}, root hints changed:\n")
+      Mix.shell().info(" - updates include:")
       Mix.shell().info(new_hints)
       Mix.shell().info(old_hints)
     end
@@ -116,6 +132,14 @@ defmodule Mix.Tasks.Iana.Update do
       body
     else
       metadata -> {:error, "#{inspect(metadata)}"}
+    end
+  end
+
+  @spec get_serial(String.t()) :: String.t()
+  defp get_serial(body) do
+    case Regex.run(~r/version\s+of\s+root\s+zone:\s*(\d+)/, body) do
+      nil -> ""
+      list -> List.last(list)
     end
   end
 
@@ -156,11 +180,28 @@ defmodule Mix.Tasks.Iana.Update do
     end
   end
 
-  @spec get_serial(String.t()) :: String.t()
-  defp get_serial(body) do
-    case Regex.run(~r/version\s+of\s+root\s+zone:\s*(\d+)/, body) do
-      nil -> ""
-      list -> List.last(list)
+  defp check(name, checksum) do
+    Mix.shell().info(" + fetching #{name}")
+    dta = fetch("#{@trust_url}/#{name}")
+    Mix.shell().info("   - retrieved #{byte_size(dta)} bytes")
+
+    if verify_checksum(:sha256, dta, checksum) do
+      Mix.shell().info("   - checksum is ok")
+      Mix.shell().info("   - saving #{name}")
+      path = Path.join(@priv, name)
+      File.write!(path, dta)
+    else
+      Mix.shell().info("   - checksum failed!")
+      Mix.shell().info("   - ignoring #{name}")
     end
+  end
+
+  @spec verify_checksum(atom, String.t(), String.t()) :: boolean
+  defp verify_checksum(type, dta, chksum) do
+    chks =
+      :crypto.hash(type, dta)
+      |> Base.encode16(case: :lower)
+
+    chks == String.downcase(chksum)
   end
 end
