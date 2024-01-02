6785a75c8c8fb88fbf7c31f6a2fa684fc18d8ae3
hertogp <git.hertogp@gmail.com>
2024-01-02 19:42:33 +0100

    codespell

--- lib/dns/msg.ex
+++ lib/dns/msg.ex
@@ -230,7 +230,7 @@ defmodule DNS.Msg do
   """
   @spec encode(t) :: {:ok, t} | {:error, DNS.MsgError.t()}
   def encode(%__MODULE__{} = msg) do
-    # donot assume qd/an/ns/ad counters are set properly!
+    # do not assume qd/an/ns/ad counters are set properly!
     lengths = [
       qdc: length(msg.question),
       anc: length(msg.answer),
